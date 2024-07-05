cc72a4feed3c18154fcd07462d1628f5d08f09a4
FFkathiriya999 <mehul.kathiriya@furnishedfinder.com>
2024-07-05 14:56:01 +0530

    Add project files.

---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using Demo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        GetData nodeDB = new GetData();
+        private readonly DemoEntities _context = new DemoEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult List()
+        {
+            var nodes = _context.tblNodes.ToList();
+            return Json(nodes, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Add(tblNode node)
+        {
+            node.startDate = DateTime.Now;
+            node.isActive = true;
+            _context.tblNodes.Add(node);
+            _context.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetbyID(int ID)
+        {
+            var Employee = _context.tblNodes.Find(ID);
+            return Json(Employee, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(tblNode node)
+        {
+            _context.Entry(node).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int ID)
+        {
+            var node = _context.tblNodes.SingleOrDefault(x => x.nodeId == ID);
+            _context.tblNodes.Remove(node);
+            _context.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult gettreelist()
+        {
+            var oficio = new SqlParameter
+            {
+                ParameterName = "pOficio",
+                Value = "0001"
+            };
+            var datas = _context.Database.SqlQuery<tblNode>("exec getAllNode @pOficio", oficio).ToList();
+            var data = GetData.BuildTree(datas);
+            
+            var json = JsonConvert.SerializeObject(data);
+            return Json(datas, JsonRequestBehavior.AllowGet);
+        }
+        
+        public class TreeNode
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public List<TreeNode> Children { get; set; }
+
+            public TreeNode()
+            {
+                Children = new List<TreeNode>();
+            }
+        }
+
+        public ActionResult About()
+        {
+            var oficio = new SqlParameter
+            {
+                ParameterName = "pOficio",
+                Value = "0001"
+            };
+            var datas= _context.Database.SqlQuery<tblNode>("exec getAllNode @pOficio", oficio).ToList();
+            var data = GetData.BuildTree(datas);
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Demo/Models/GetData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using static Demo.Controllers.HomeController;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Xml.Linq;
+
+namespace Demo.Models
+{
+    public class GetData
+    {
+        public static TreeNode BuildTree(List<tblNode> items)
+        {
+            // Create a dictionary to store nodes by their IDs for fast lookup
+            var nodeDictionary = new Dictionary<int, TreeNode>();
+
+            // Create tree nodes for each item in the list
+            foreach (var item in items)
+            {
+                var node = new TreeNode { Id = item.nodeId, Name = item.nodeName };
+                nodeDictionary.Add(item.nodeId, node);
+            }
+
+            // Build the tree structure
+            TreeNode root = null;
+            foreach (var item in items)
+            {
+                TreeNode node = nodeDictionary[item.nodeId];
+                if (item.parentNodeId == 0)
+                {
+                    // If ParentId is 0, it's the root node
+                    root = node;
+                }
+                else
+                {
+                    // Find parent node and add current node as its child
+                    TreeNode parent = nodeDictionary[item.parentNodeId];
+                    parent.Children.Add(node);
+                }
+            }
+
+            return root;
+        }
+    }
+
+}
